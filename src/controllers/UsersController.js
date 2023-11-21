7e1b738d9d409aec39c5bcb970b199f591fd0095
denilsonbaptista <baptistadenilsonbp@gmail.com>
2023-11-21 20:31:38 -0300

    Controllers

---
+++ src/controllers/UsersController.js
@@ -0,0 +1,11 @@
+class UsersController {
+  /**
+   * index - GET para lista vários registros.
+   * show - GET para exibir um registro especifico.
+   * create -  POST para criar um registro.
+   * update - PUT para atualizar um registro.
+   * delete - DELETE para remover um registro.
+   */
+}
+
+module.exports = UsersController
