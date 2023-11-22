54f80de3fdecb893652aa29a3ab088c0eaf8591e
denilsonbaptista <baptistadenilsonbp@gmail.com>
2023-11-21 21:24:41 -0300

    App Error

--- src/routes/users.routes.js
+++ src/routes/users.routes.js
@@ -4,18 +4,8 @@ const UsersController = require("../controllers/UsersController")
 
 const usersRoutes = Router()
 
-function myMiddlewares(request, response, next) {
-  console.log("Você passou pelo Meddleware")
-
-  if (!request.body.isAdmin) {
-    return response.json({ message: "user unauthorized" })
-  }
-
-  next()
-}
-
 const usersController = new UsersController()
 
-usersRoutes.post("/", myMiddlewares, usersController.create)
+usersRoutes.post("/", usersController.create)
 
 module.exports = usersRoutes
